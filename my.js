e8bcabb234c02ff40ca2ff7089324a30f845114e
Matt Bulwan <matt.bulwan@sonarsource.com>
2026-06-09 11:03:05 -0500

    Update my.js (#29)

--- my.js
+++ my.js
@@ -17,6 +17,6 @@ greetUser("Bob");
 
 // Demonstrate a simple arithmetic operation.
 let num1 = 10;
-let num2 = 5;
+let num2 = 15;
 let sum = num1 + num2;
 console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
